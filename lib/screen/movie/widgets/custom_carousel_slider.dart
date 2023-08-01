97b9ed44ff2b70a575dabe0669fce93ad35d04ec
asment <asment.tan@hexainitiative.com>
2023-08-01 21:17:46 +0800

    [] (asment) Modify custom carousel slide

--- lib/screen/movie/widgets/custom_carousel_slider.dart
+++ lib/screen/movie/widgets/custom_carousel_slider.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/models/movie.dart';
 import 'package:movie_app/screen/movie/widgets/landing_card.dart';
@@ -9,27 +10,32 @@ class CustomCarouselSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    return SizedBox(
-      width: size.width,
-      height: (size.height * 0.33 < 300) ? 300 : size.height * 0.33,
-      child: PageView.builder(
-        scrollDirection: Axis.horizontal,
-        physics: const BouncingScrollPhysics(),
-        pageSnapping: true,
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          var url = movies[index].backdropPath.toString();
-          return GestureDetector(
-            onTap: () {},
-            child: LandingCard(
-              CachedNetworkImageProvider(
-                "https://image.tmdb.org/t/p/original$url",
-              ),
-              movies[index].title.toString(),
+    final size = MediaQuery.of(context).size;
+    return CarouselSlider.builder(
+      itemCount: movies.length,
+      itemBuilder: (context, index, realIndex) {
+        final url = movies[index].backdropPath.toString();
+        return GestureDetector(
+          onTap: () {},
+          child: LandingCard(
+            CachedNetworkImageProvider(
+              "https://image.tmdb.org/t/p/original$url",
             ),
-          );
-        },
+            movies[index].title.toString(),
+          ),
+        );
+      },
+      options: CarouselOptions(
+        height: (size.height * 0.33 < 300) ? 300 : size.height * 0.33,
+        aspectRatio: 16 / 9,
+        viewportFraction: 1,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        reverse: false,
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
       ),
     );
   }
